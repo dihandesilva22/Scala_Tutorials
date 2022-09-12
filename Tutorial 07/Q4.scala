6af413364396c454aa62b6710b68b6b9ce972a61
dihandesilva22 <84619964+dihandesilva22@users.noreply.github.com>
2022-09-13 00:42:18 +0530

    Tutorial 07 Added

--- Tutorial 07/Q4.scala
+++ Tutorial 07/Q4.scala
@@ -3,27 +3,28 @@ class Account(id:String,accNo:String,bal:Double){
     var accountNo:String = accNo;
     var balance = bal;
 
-    def transfer(toAcc:Account,bal:Double) = {
-        if(this.balance>=bal){
-            this.balance = this.balance-bal;
-            toAcc.balance = toAcc.balance+bal;
-        }else{
-            println("Balance is not sufficient");
-        }
-    }
-
+    def +(othr:Account):Account = new Account("","",this.balance+othr.balance)  
     override def toString() = "NIC: "+nic+" Account No: "+accountNo+" Balance: "+balance;
 
 
 }
 
 object Q4 extends App{
-    var bank:List[Account] = List();
+  
+    var a1 = new Account("1234","ER-23123",56000);       //Sample Data
+    var a2 = new Account("3453","ER-33113",96000);
+    var a3 = new Account("3458","ER-34513",-10000);
+    var a4 = new Account("3673","ER-38813",-1000);
+    var bank:List[Account] = List(a1,a2,a3,a4);
 
     val overDraft = (bnk:List[Account]) => bnk.filter((x:Account)=>x.balance<0)
-    val totSum = (bnk:List[Account]) => bnk.reduce((x,y)=>x.balance+y.balance)
-
-    val balWithInterest = (x:Account) => if(x.balance<0) -x.balance*100.1 else x.balance*100.05
+    val totSum = (bnk:List[Account]) => bnk.reduce((x,y)=>x+y)
+    val balWithInterest = (x:Account) => if(x.balance<0) x.balance*(100.1/100) else x.balance*(100.05/100)
     val finalBal = (bnk:List[Account]) => bnk.map(balWithInterest)
+  
+    println(overDraft(bank))
+    println(totSum(bank).balance)
+    println(finalBal(bank))
 
 }
+
