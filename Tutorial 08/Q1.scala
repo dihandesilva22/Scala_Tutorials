74d822b7760d343b47c428c4a0aff51ab7f6ce61
dihandesilva22 <84619964+dihandesilva22@users.noreply.github.com>
2022-09-20 01:07:47 +0530

    Tutorial 08 Added

--- Tutorial 08/Q1.scala
+++ Tutorial 08/Q1.scala
@@ -1,19 +1,21 @@
-case class Point(x:Double, y:Double){
+case class Point(var x:Double, var y:Double){
 
   def add(othr:Point):Point = {
     Point(this.x+othr.x,this.y+othr.y)
   }
 
   def move(dx:Double,dy:Double):Point = {
-    Point(x+dx,y+dy)
+    Point(this.x+dx,this.y+dy)
   }
 
   def distance(othr:Point):Double = {
     scala.math.sqrt(scala.math.pow((this.x-othr.x),2) + scala.math.pow((this.y-othr.y),2))
   }
 
-  def invert():Point = {
-    Point(this.y,this.x)
+  def invert():Unit = {
+    var temp = this.y;
+    this.y = x;
+    this.x = temp;
   }
 
 }
@@ -26,6 +28,7 @@ object Q1 extends App{
     println(p1.add(p2))
     println(p1.distance(p2))
     println(p1.move(3,3))
-    println(p2.invert())
+    p2.invert()
+    println(p2)
 
 }
